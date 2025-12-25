7dc50dd1303521451a84017a306faf4355acb000
Gabriel Eguiguren <gabo.egui@gmail.com>
2025-12-25 18:37:35 -0500

    WebEnvironment.MOCK

--- src/test/java/com/egui/gabo/webflux/api/ProductHandlerUnitTests.java
+++ src/test/java/com/egui/gabo/webflux/api/ProductHandlerUnitTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -17,8 +18,15 @@ import com.egui.gabo.webflux.api.service.ProductService;
 
 import reactor.core.publisher.Mono;
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+/**
+ * Unit tests for @ProductHandler
+ * 
+ * Uses WebEnvironment.MOCK to save resources
+ * 
+ * @author Gabriel Eguiguren P.
+ */
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK )
 class ProductHandlerUnitTests {
 	
 	
